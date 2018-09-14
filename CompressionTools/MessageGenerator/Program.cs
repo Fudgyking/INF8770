a13508763a3ec96d7b9827f35540669989e341c9
Vincent <vincent.chasse@polymtl.ca>
2018-09-14 17:26:02 -0400

    Modif du code

--- CompressionTools/MessageGenerator/Program.cs
+++ CompressionTools/MessageGenerator/Program.cs
@@ -11,7 +11,8 @@ namespace MessageGenerator
     {
         static void Main(string[] args)
         {
-            int length = 0, ghetto = 0, percent = 0;
+            int length = 0, type = 0;
+            double percent = 0;
             string str = "", outFile = "";
 
             for (int i = 0; i < args.Length; i++)
@@ -31,25 +32,27 @@ namespace MessageGenerator
                         i++;
                         outFile = args[i];
                         break;
-                    case "-ghetto":
+                    case "-t":
                         i++;
-                        ghetto = int.Parse(args[i]);
+                        type = int.Parse(args[i]);
                         break;
-                    case "p":
+                    case "-p":
                         i++;
-                        percent = int.Parse(args[i]);
+                        percent = double.Parse(args[i]);
                         break;
                     default:
                         break;
                 }
             }
 
-            if(ghetto == 0)
+            if(type == 0)
             {
                 generateMessage(length, str, outFile);
             }
-            else if(ghetto == 1)
+            else if(type == 1)
             {
+                Console.WriteLine(percent);
+
                 generateMessageBinary(length, percent / 100.0, outFile);
             }
         }
@@ -70,12 +73,17 @@ namespace MessageGenerator
             Random rand = new Random();
             for(int i = 0; i < length; i++)
             {
-                int num = rand.Next();
-                if(num == 0)
+                double num = rand.NextDouble();
+                if(num < percent0)
                 {
+                    message.Append(0);
                     compteur++;
                 }
-                message.Append(num);
+                else
+                {
+                    message.Append(1);
+                }
+
             }
 
             File.WriteAllText(Directory.GetCurrentDirectory() + "\\" + outFile, message.ToString());
