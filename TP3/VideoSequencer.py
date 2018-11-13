ef63c7998e364f595f29037ce3e7f7b7cbaf0924
Fudgyking <escalado19@hotmail.com>
2018-11-13 17:39:37 -0500

    Grayscale

--- TP3/VideoSequencer.py
+++ TP3/VideoSequencer.py
@@ -38,7 +38,6 @@ frame = 0
 if videoCapture.isOpened :
     # Lecture de la premiere frame
     reading, image = videoCapture.read()
-    prevImage = copy.deepcopy(image)
 
     seuilCoupure = 2500
     seuilEffet = 445
@@ -59,50 +58,51 @@ if videoCapture.isOpened :
         distanceR = distance(histogramR, prevhistogramR)
         distanceG = distance(histogramG, prevhistogramG)
         distanceB = distance(histogramB, prevhistogramB)
-        #print("Distance R: ", distanceR)
-        #print("Distance G: ", distanceG)
-        #print("Distance B: ", distanceB)
+
         totalDistance = 2 * distanceR + distanceG / 2 + distanceB / 2
         #fou seuil trouvé expérimentalement
         if  totalDistance > seuilCoupure:
             evaluatingEffect = False
             print("Coupure à {}, distance de {}".format(frame,totalDistance))
-            #print("Distance R: ", distanceR)
-            #print("Distance G: ", distanceG)
-            #print("Distance B: ", distanceB)
-            cv2.imshow("Coupure", image)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-               break
+
+            # cv2.imshow("Coupure", image)
+            # if cv2.waitKey(1) & 0xFF == ord('q'):
+            #    break
         elif totalDistance > seuilEffet:
             if not evaluatingEffect:
                 evaluatingEffect = True
                 effectStart = frame
                 effectStartImage = [histogramR,histogramG,histogramB]
-                cv2.imshow("EffetStart", image)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
+                # cv2.imshow("EffetFin", image)
+                # if cv2.waitKey(1) & 0xFF == ord('q'):
+                #     break
         else:
             if evaluatingEffect:
                 distanceR = distance(histogramR, effectStartImage[0])
                 distanceG = distance(histogramG, effectStartImage[1])
                 distanceB = distance(histogramB, effectStartImage[2])
-                #print("Distance R: ", distanceR)
-                #print("Distance G: ", distanceG)
-                #print("Distance B: ", distanceB)
+
                 totalDistance = 2 * distanceR + distanceG / 2 + distanceB / 2
                 if  totalDistance > 2300:
                     print("Potentiel début effet à {}, distance de {}".format(effectStart,totalDistance))
                     print("Potentiel fin d'effet à {}, distance de {}".format(frame,totalDistance))
-                    cv2.imshow("EffetFin", image)
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
+                    # cv2.imshow("EffetFin", image)
+                    # if cv2.waitKey(1) & 0xFF == ord('q'):
+                    #     break
                 evaluatingEffect = False
 
-        #cv2.imshow("Video", image)
 
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-        #    break
+
+
+        grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        grayImageBlur = cv2.GaussianBlur(grayImage,(5,5),3)
+        cv2.imshow("Gray", grayImage)
+        diffGauss = np.absolute(grayImage - grayImageBlur)
+        diffGauss = diffGauss * 255/np.max(diffGauss)
+        cv2.imshow("GrayBlur", diffGauss)
         
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
         end = time.time()
 
         #print(frame, " : ", totalDistance)
